b7a9221ceedc216100045a74bf1543d6ae93b33e
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2021-07-30 18:01:36 -0400

    Move configure call to configure.swift (#4)

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -8,10 +8,6 @@ try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 try configure(app)
 
-// Configure the app to connect to a MongoDB deployment. If a connection string is provided via the `MONGODB_URI`
-// environment variable it will be used; otherwise, use the default connection string for a local MongoDB server.
-try app.mongoDB.configure(Environment.get("MONGODB_URI") ?? "mongodb://localhost:27017")
-
 defer {
     // Cleanup the application's MongoDB data.
     app.mongoDB.cleanup()
